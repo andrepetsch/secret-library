a9f9cbde934b4fa2481142b70c7d822dbda9ca1b
Andre Petsch <andre.petsch@outlook.com>
2025-11-07 17:27:49 +0100

    add commented import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+// import MicrosoftEntraIDProvider from "next-auth/providers/microsoft-entra-id"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
 
